4066f248e6fc450cdda1036e227f8c87f6115851
pdesic <pavledesic96@gmail.com>
2020-05-21 17:50:26 +0200

    Added ViewModel for Movies

---
+++ MovieApp/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MovieApp.Models;
+
+
+namespace MovieApp.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public string MovieTrailerUrl { get; set; }
+        public string ImagePath { get; set; }
+
+        public MovieFormViewModel()
+        {
+            ObjectId id = ObjectId.GenerateNewId();
+
+            Id = id.ToString();
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            Genre = movie.Genre;
+            MovieTrailerUrl = movie.MovieTrailerUrl;
+            ImagePath = movie.ImagePath;
+        }
+    }
+}
