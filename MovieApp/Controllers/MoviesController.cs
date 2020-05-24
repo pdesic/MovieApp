2bd0a056ff8298d4f791f2e4873d21f58cbcff1d
mare22 <mare22>
2020-05-24 19:02:42 +0200

    Multiple select and uploading image works fine

--- MovieApp/Controllers/MoviesController.cs
+++ MovieApp/Controllers/MoviesController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using MovieApp.Models;
 using MovieApp.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MovieApp.ViewModels;
 
 namespace MovieApp.Controllers
@@ -13,11 +18,13 @@ namespace MovieApp.Controllers
     {
         private readonly MovieService _movieService;
         private readonly GenreService _genreService;
+        private IHostingEnvironment _env;
 
-        public MoviesController(MovieService movieService, GenreService genreService)
+        public MoviesController(MovieService movieService, GenreService genreService, IHostingEnvironment environment)
         {
             _movieService = movieService;
             _genreService = genreService;
+            _env = environment;
         }
 
         [HttpGet]
@@ -41,9 +48,7 @@ namespace MovieApp.Controllers
                 Genre = _genreService.GetFirst(),
                 Movie = new Movie()
             };    
-            // var genres = _genreService.GetFirst();
-            // var movies = new Movie();
-
+            
             return View(viewModel);
         }
 
@@ -63,11 +68,32 @@ namespace MovieApp.Controllers
 
             return View();
         }
-
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult<Movie> Create(Movie movie)
+        public async Task<IActionResult> Create(Movie movie, IFormFile image)
         {
+            // If image uploaded, make image name unique and store into "\wwwroot\images" folder,
+            // otherwise set validation error message
+            if (image != null && image.Length > 0)
+            {
+                string uniqueImageName = Guid.NewGuid().ToString() + "_" + image.FileName;
+
+                string imageFullPath = System.IO.Directory.GetCurrentDirectory() + @"\wwwroot\images\" + uniqueImageName;
+                
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    await image.CopyToAsync(stream);
+                }
+
+                movie.ImagePath = uniqueImageName;
+
+            }
+            else
+            {
+                ModelState.AddModelError("Movie.ImagePath", "Please upload image");
+            }
+            
             if (!ModelState.IsValid)
             {
                 var viewModel = new MovieFormViewModel
@@ -75,14 +101,15 @@ namespace MovieApp.Controllers
                     Genre = _genreService.GetFirst(),
                     Movie = new Movie()
                 };   
-
+            
                 return View("CreateForm", viewModel);
             }
             
             _movieService.Create(movie);
-
+            
             return RedirectToAction("Index","Movies");
         }
+
 
         [HttpPut]
         public ActionResult<Movie> Update(string id, Movie movieIn)
