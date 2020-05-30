e34b0c3985ac006f8d62d99c0dd10e7015731fd5
pdesic <pavledesic96@gmail.com>
2020-05-30 23:48:55 +0200

    fixed some bugs

--- MovieApp/Models/Movie.cs
+++ MovieApp/Models/Movie.cs
@@ -36,10 +36,10 @@ namespace MovieApp.Models
 
         [Display(Name = "Description")]
         public string Plot { get; set; }
-        [AllowNull]
+ 
         [Range(1,10)]
         public int[] Rating { get; set; }
-        [AllowNull]
+        
         public string[] UsersThatRatedMovie { get; set; }
     }
 }
