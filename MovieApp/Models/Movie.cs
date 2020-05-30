0b5e8de2155d090b0e0c778d12bc80d6fe290ec1
pdesic <pavledesic96@gmail.com>
2020-05-31 01:03:04 +0200

    aaa

--- MovieApp/Models/Movie.cs
+++ MovieApp/Models/Movie.cs
@@ -36,10 +36,10 @@ namespace MovieApp.Models
 
         [Display(Name = "Description")]
         public string Plot { get; set; }
- 
+        
         [Range(1,10)]
         public int[] Rating { get; set; }
-        
+
         public string[] UsersThatRatedMovie { get; set; }
     }
 }
